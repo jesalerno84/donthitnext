921cd417a5ff00c0f65d3c7f396a7135d2608711
jeff <jesalerno84@gmail.com>
2016-05-25 21:36:17 -0400

    set up mocha

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('chai').should;
+should();
+
+describe('test', function () {
+    it('should assert 1 = 1', function () {
+        var i = 1;
+        i.should.equal(1);
+    });
+});
